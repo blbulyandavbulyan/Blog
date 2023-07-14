9c472f94b1528705c1a986f2aec8d321b03e2f29
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 17:29:36 +0400

    Added GeneralConfig

---
+++ src/main/java/org/blbulyandavbulyan/blog/configs/GeneralConfig.java
@@ -0,0 +1,9 @@
+package org.blbulyandavbulyan.blog.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.blbuluyandavbulyan.blog")
+public class GeneralConfig {
+}
