7b14fd378b1fa4e7c2cf25350e99e78d51f3d261
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 12:24:08 +0400

    Added @Repository annotation for ArticleRepository

--- src/main/java/org/blbulyandavbulyan/blog/repositories/ArticleRepository.java
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/ArticleRepository.java
@@ -2,6 +2,8 @@ package org.blbulyandavbulyan.blog.repositories;
 
 import org.blbulyandavbulyan.blog.entities.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
