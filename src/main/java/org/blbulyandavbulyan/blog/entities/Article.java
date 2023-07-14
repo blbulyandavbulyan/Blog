13a98befb2b807c2b434b1e1475a7a55befded8f
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 12:47:13 +0400

    added getter and setter annotations for Article

--- src/main/java/org/blbulyandavbulyan/blog/entities/Article.java
+++ src/main/java/org/blbulyandavbulyan/blog/entities/Article.java
@@ -1,12 +1,16 @@
 package org.blbulyandavbulyan.blog.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.ZonedDateTime;
 import java.util.List;
 
 @Entity
 @Table(name = "articles")
+@Getter
+@Setter
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
