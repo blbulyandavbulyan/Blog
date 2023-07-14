9959b14fb386df5e3187825ce75f04a6e9686683
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 18:30:28 +0400

    Added method existsByName in UserRepository

--- src/main/java/org/blbulyandavbulyan/blog/repositories/UserRepository.java
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByName(String username);
+    boolean existsByName(String username);
 }
