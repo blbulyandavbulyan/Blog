2ba8aae4f498c397906260da5f924112c7e25038
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 20:32:09 +0400

    Added processing for publishing article in ArticlesController

--- src/main/java/org/blbulyandavbulyan/blog/controllers/ArticlesController.java
+++ src/main/java/org/blbulyandavbulyan/blog/controllers/ArticlesController.java
@@ -1,12 +1,16 @@
 package org.blbulyandavbulyan.blog.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.blbulyandavbulyan.blog.dtos.ArticleForPublishing;
 import org.blbulyandavbulyan.blog.exceptions.AppError;
 import org.blbulyandavbulyan.blog.exceptions.ArticleNotFoundException;
+import org.blbulyandavbulyan.blog.exceptions.UserNotFoundException;
 import org.blbulyandavbulyan.blog.services.ArticlesService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +25,17 @@ public class ArticlesController {
         }
         catch (ArticleNotFoundException e){
             return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), e.getMessage()), HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/publish")
+    @ResponseBody
+    public ResponseEntity<?> publishArticle(@RequestBody ArticleForPublishing articleForPublishing, Principal principal){
+        try {
+            articlesService.publishArticle(articleForPublishing, principal.getName());
+            return ResponseEntity.ok("article was published!");
+        }
+        catch (UserNotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
     }
 }
