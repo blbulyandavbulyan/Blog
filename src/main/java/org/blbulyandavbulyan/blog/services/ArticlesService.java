ec98c24f24195fa6eb74d4c89573a3c110b44c1e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 20:31:48 +0400

    Added publishArticle overloading in ArticlesService

--- src/main/java/org/blbulyandavbulyan/blog/services/ArticlesService.java
+++ src/main/java/org/blbulyandavbulyan/blog/services/ArticlesService.java
@@ -2,6 +2,7 @@ package org.blbulyandavbulyan.blog.services;
 
 import lombok.RequiredArgsConstructor;
 import org.blbulyandavbulyan.blog.dtos.ArticleDto;
+import org.blbulyandavbulyan.blog.dtos.ArticleForPublishing;
 import org.blbulyandavbulyan.blog.entities.Article;
 import org.blbulyandavbulyan.blog.entities.User;
 import org.blbulyandavbulyan.blog.exceptions.ArticleNotFoundException;
@@ -14,6 +15,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ArticlesService {
     private final ArticleRepository articleRepository;
+    private final UserService userService;
     public void publishArticle(String title, String text, User publisher){
         Article article = new Article();
         article.setTitle(title);
@@ -24,5 +26,10 @@ public class ArticlesService {
 
     public ArticleDto getById(Long id) {
         return articleRepository.findArticleDtoById(id).orElseThrow(()->new ArticleNotFoundException("Article with id " + id + " not found"));
+    }
+
+    public void publishArticle(ArticleForPublishing articleForPublishing, String publisherName) {
+        User user = userService.findByName(publisherName);
+        publishArticle(articleForPublishing.title(), articleForPublishing.text(), user);
     }
 }
