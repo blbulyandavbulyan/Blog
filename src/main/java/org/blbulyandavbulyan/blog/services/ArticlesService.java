d2c25ba0fbf070c2e9521fb3180156d73977e8fc
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 20:02:38 +0400

    Added ArticlesService

---
+++ src/main/java/org/blbulyandavbulyan/blog/services/ArticlesService.java
@@ -0,0 +1,28 @@
+package org.blbulyandavbulyan.blog.services;
+
+import lombok.RequiredArgsConstructor;
+import org.blbulyandavbulyan.blog.dtos.ArticleDto;
+import org.blbulyandavbulyan.blog.entities.Article;
+import org.blbulyandavbulyan.blog.entities.User;
+import org.blbulyandavbulyan.blog.exceptions.ArticleNotFoundException;
+import org.blbulyandavbulyan.blog.repositories.ArticleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ArticlesService {
+    private final ArticleRepository articleRepository;
+    public void publishArticle(String title, String text, User publisher){
+        Article article = new Article();
+        article.setTitle(title);
+        article.setText(text);
+        article.setPublisher(publisher);
+        articleRepository.save(article);
+    }
+
+    public ArticleDto getById(Long id) {
+        return articleRepository.findArticleDtoById(id).orElseThrow(()->new ArticleNotFoundException("Article with id " + id + " not found"));
+    }
+}
