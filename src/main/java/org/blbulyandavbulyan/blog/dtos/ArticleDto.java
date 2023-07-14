5785477592d6a021027b88fbe291da7132a7e8e0
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 19:05:28 +0400

    ArticleDto added

---
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/ArticleDto.java
@@ -0,0 +1,10 @@
+package org.blbulyandavbulyan.blog.dtos;
+
+/**
+ * Данный класс предназначен для отображения пользователю статьи
+ * @param title название статьи
+ * @param text текст стать
+ * @param authorName имя автора данной статьи
+ */
+public record ArticleDto(String title, String text, String authorName) {
+}
