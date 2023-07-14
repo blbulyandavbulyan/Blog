8dc75ca4735c5f23c3f46d3da9bb7778f623ebe0
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 13:59:21 +0400

    Added publish_date to comments table

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,3 +1,3 @@
 CREATE TABLE users(user_id bigserial PRIMARY KEY, name VARCHAR(255) NOT NULL UNIQUE, registration_date TIMESTAMP WITH TIME ZONE NOT NULL);
 CREATE TABLE articles(article_id bigserial PRIMARY KEY, author_id INTEGER, text VARCHAR(5000) NOT NULL, publish_date TIMESTAMP WITH TIME ZONE NOT NULL);
-CREATE TABLE comments(comment_id bigserial PRIMARY KEY, author_id INTEGER, article_id INTEGER, text VARCHAR(2000) NOT NULL);
+CREATE TABLE comments(comment_id bigserial PRIMARY KEY, author_id INTEGER, article_id INTEGER, text VARCHAR(2000) NOT NULL, publish_date TIMESTAMP WITH TIME ZONE NOT NULL);
