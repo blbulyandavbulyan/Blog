a6005c8ef3675e9e4a4c28518e3c5087430cd8c9
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 20:44:57 +0400

    Now publishArticle returns ArticlePublished

--- src/main/java/org/blbulyandavbulyan/blog/services/ArticlesService.java
+++ src/main/java/org/blbulyandavbulyan/blog/services/ArticlesService.java
@@ -3,33 +3,33 @@ package org.blbulyandavbulyan.blog.services;
 import lombok.RequiredArgsConstructor;
 import org.blbulyandavbulyan.blog.dtos.ArticleDto;
 import org.blbulyandavbulyan.blog.dtos.ArticleForPublishing;
+import org.blbulyandavbulyan.blog.dtos.ArticlePublished;
 import org.blbulyandavbulyan.blog.entities.Article;
 import org.blbulyandavbulyan.blog.entities.User;
 import org.blbulyandavbulyan.blog.exceptions.ArticleNotFoundException;
 import org.blbulyandavbulyan.blog.repositories.ArticleRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class ArticlesService {
     private final ArticleRepository articleRepository;
     private final UserService userService;
-    public void publishArticle(String title, String text, User publisher){
+    public ArticlePublished publishArticle(String title, String text, User publisher){
         Article article = new Article();
         article.setTitle(title);
         article.setText(text);
         article.setPublisher(publisher);
         articleRepository.save(article);
+        return new ArticlePublished(article.getArticleId());
     }
 
     public ArticleDto getById(Long id) {
         return articleRepository.findArticleDtoById(id).orElseThrow(()->new ArticleNotFoundException("Article with id " + id + " not found"));
     }
 
-    public void publishArticle(ArticleForPublishing articleForPublishing, String publisherName) {
+    public ArticlePublished publishArticle(ArticleForPublishing articleForPublishing, String publisherName) {
         User user = userService.findByName(publisherName);
-        publishArticle(articleForPublishing.title(), articleForPublishing.text(), user);
+        return publishArticle(articleForPublishing.title(), articleForPublishing.text(), user);
     }
 }
