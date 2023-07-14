e8db492529321297b65a91f2ef01eddfbfbcb8a9
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 12:15:36 +0400

    Added fields to Comment, now Comment is entity

--- src/main/java/org/blbulyandavbulyan/blog/entities/Comment.java
+++ src/main/java/org/blbulyandavbulyan/blog/entities/Comment.java
@@ -1,4 +1,24 @@
 package org.blbulyandavbulyan.blog.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+    @OneToOne
+    private User author;
+    @Column(name = "text", length = 5000)
+    private String text;
+    @Column(name = "publish_date")
+    private ZonedDateTime publishDate;
 }
