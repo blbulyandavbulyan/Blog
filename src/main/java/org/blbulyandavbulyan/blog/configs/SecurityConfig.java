aef632a4960980a801123046c27c26bb2041f3ad
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 15:11:21 +0400

    Added UserDetailsService

--- src/main/java/org/blbulyandavbulyan/blog/configs/SecurityConfig.java
+++ src/main/java/org/blbulyandavbulyan/blog/configs/SecurityConfig.java
@@ -1,7 +1,10 @@
 package org.blbulyandavbulyan.blog.configs;
 
+import org.blbulyandavbulyan.blog.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -10,5 +13,9 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(@Autowired UserService userService) {
+        return userService;
     }
 }
