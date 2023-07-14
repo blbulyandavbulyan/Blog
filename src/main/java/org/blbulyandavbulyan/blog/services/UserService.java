05aace3cbb93ec88371dc17adb17414e3308f207
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 14:14:39 +0400

    Added simple user service

---
+++ src/main/java/org/blbulyandavbulyan/blog/services/UserService.java
@@ -0,0 +1,25 @@
+package org.blbulyandavbulyan.blog.services;
+
+import org.blbulyandavbulyan.blog.entities.User;
+import org.blbulyandavbulyan.blog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+    public User registerUser(String userName, String password){
+        User user = new User(userName, passwordEncoder.encode(password));
+        userRepository.save(user);
+        return user;
+    }
+}
