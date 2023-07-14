d52329030fa4cf431e420816fcb75f68bf78774f
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 12:23:41 +0400

    Added UserRepository

---
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package org.blbulyandavbulyan.blog.repositories;
+
+import org.blbulyandavbulyan.blog.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
