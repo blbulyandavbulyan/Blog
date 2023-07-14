7ed7a173d941ac4d364c289a902dd6c40a971841
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 12:19:41 +0400

    added fields to Article

--- src/main/java/org/blbulyandavbulyan/blog/entities/Article.java
+++ src/main/java/org/blbulyandavbulyan/blog/entities/Article.java
@@ -1,4 +1,23 @@
 package org.blbulyandavbulyan.blog.entities;
 
+import jakarta.persistence.*;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "articles")
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_id")
+    private Long id;
+    @ManyToOne
+    private User publisher;
+    @Column(name = "text", length = 2000)
+    private String text;
+    @OneToMany
+    private List<Comment> comments;
+    @Column(name = "publish_date")
+    private ZonedDateTime publishDate;
 }
