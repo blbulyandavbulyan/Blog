d6fa06cb65af0dc2ecf637e3db5273b7ad400546
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-14 20:01:44 +0400

    changed successful response format for registerUser method in AuthCController

--- src/main/java/org/blbulyandavbulyan/blog/controllers/AuthController.java
+++ src/main/java/org/blbulyandavbulyan/blog/controllers/AuthController.java
@@ -38,7 +38,7 @@ public class AuthController {
     public ResponseEntity<?> registerUser(@RequestBody RegistrationUser registrationUser){
         if(!userService.exists(registrationUser.username())){//если пользователя не существует, можем регистрировать
             userService.registerUser(registrationUser.username(), registrationUser.password());
-            return ResponseEntity.ok("user was successfully registered");
+            return new ResponseEntity<>("user was successfully registered!", HttpStatus.CREATED);
         }
         else return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "User already registered!"), HttpStatus.BAD_REQUEST);
     }
