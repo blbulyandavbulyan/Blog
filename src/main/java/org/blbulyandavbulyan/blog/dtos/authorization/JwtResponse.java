68d190e3b27e19f7d477572c5d15abc227950c6b
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-15 10:20:14 +0400

    javadoc added

--- src/main/java/org/blbulyandavbulyan/blog/dtos/authorization/JwtResponse.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/authorization/JwtResponse.java
@@ -1,4 +1,8 @@
 package org.blbulyandavbulyan.blog.dtos.authorization;
 
+/**
+ * Данная запись отправляется в виде JSON в случае успешной JWT авторизации
+ * @param token JWT токен, который будет использоваться для идентификации пользователя
+ */
 public record JwtResponse(String token) {
 }
