18ccf6bb259c5ed84bff9bd998262a0d888e652e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-15 10:14:49 +0400

    javadoc added

--- src/main/java/org/blbulyandavbulyan/blog/exceptions/AppError.java
+++ src/main/java/org/blbulyandavbulyan/blog/exceptions/AppError.java
@@ -2,6 +2,12 @@ package org.blbulyandavbulyan.blog.exceptions;
 
 import java.util.Date;
 
+/**
+ * Данная запись используется для отправки в виде JSON пользователю, в случае ошибки
+ * @param status статус код
+ * @param message сообщение
+ * @param timestamp время ошибки
+ */
 public record AppError (int status, String message, Date timestamp){
     public AppError(int status, String message) {
         this(status, message, new Date());
