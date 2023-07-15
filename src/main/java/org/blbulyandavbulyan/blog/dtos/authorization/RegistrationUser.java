fa63cd5cb72557e18c6bef7a2a11e762e420187d
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-15 10:20:54 +0400

    javadoc added

--- src/main/java/org/blbulyandavbulyan/blog/dtos/authorization/RegistrationUser.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/authorization/RegistrationUser.java
@@ -1,4 +1,9 @@
 package org.blbulyandavbulyan.blog.dtos.authorization;
 
+/**
+ * Данная запись предназначена для запроса регистрации пользователя, приходит на сервер в формате json
+ * @param username имя нового пользователя
+ * @param password пароль
+ */
 public record RegistrationUser (String username, String password){
 }
