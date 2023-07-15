53705b2f15a7e01e28fb2cbc75e36e8aafac9b3d
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-15 10:18:24 +0400

    javadoc added

--- src/main/java/org/blbulyandavbulyan/blog/dtos/article/ArticlePublished.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/article/ArticlePublished.java
@@ -1,4 +1,8 @@
 package org.blbulyandavbulyan.blog.dtos.article;
 
+/**
+ * Данная запись отправляется пользователю в качестве ответа, в случае успешной публикации статьи
+ * @param articleId ИД добавленной статьи
+ */
 public record ArticlePublished(Long articleId) {
 }
