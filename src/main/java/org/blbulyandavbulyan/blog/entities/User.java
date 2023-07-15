88d3ec37f23163bd04e980e185370889cb3c31e5
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-15 09:25:43 +0400

    Join table name for roles changed

--- src/main/java/org/blbulyandavbulyan/blog/entities/User.java
+++ src/main/java/org/blbulyandavbulyan/blog/entities/User.java
@@ -29,7 +29,7 @@ public class User implements UserDetails {
     private String passwordHash;
     @ManyToMany
     @JoinTable(
-            name = "users_privileges",
+            name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
