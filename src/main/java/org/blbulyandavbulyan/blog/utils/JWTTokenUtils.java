0ce656135b2422f5cc3c80bd1b0e6269b585fca6
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-15 11:16:22 +0400

    javadoc added

--- src/main/java/org/blbulyandavbulyan/blog/utils/JWTTokenUtils.java
+++ src/main/java/org/blbulyandavbulyan/blog/utils/JWTTokenUtils.java
@@ -18,38 +18,72 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Данный класс предназначен для удобной работы с jwt токенами
+ */
 @Component
 public class JWTTokenUtils {
+    /**
+     * Время жизни jwt токена
+     */
     @Value("${jwt.lifetime}")
     private Duration jwtLifeTime;
+    /**
+     * Ключ для подписи jwt токена
+     */
     private Key secretKey;
+    /**
+     * Парсер, для парсинга jwt токена
+     */
     private JwtParser parser;
 
+    /**
+     * Метод инициализации нашего бина
+     */
     @PostConstruct
     private void init(){
+        //задаём ключ подписи
         secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256); //or HS384 or HS512
+        //создаём парсер
         parser = Jwts.parserBuilder().setSigningKey(secretKey).build();
     }
+
+    /**
+     * Метод генерирует jwt токен
+     * @param userDetails детали пользователя, по которым будет сгенерирован данный токен
+     * @return полученный jwt токен
+     */
     public String generateToken(UserDetails userDetails){
         Map<String, Object> claims = new HashMap<>();
         List<String> rolesList = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
-        claims.put("roles", rolesList);
-        Date issuedDate = new Date();
-        Date expiredDate = new Date(issuedDate.getTime() + jwtLifeTime.toMillis());
+        claims.put("roles", rolesList);//добавляем роли
+        Date issuedDate = new Date();//время создания токена
+        Date expiredDate = new Date(issuedDate.getTime() + jwtLifeTime.toMillis());//время истечения токена
         return Jwts.builder().setClaims(claims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(issuedDate)
                 .setExpiration(expiredDate)
                 .signWith(secretKey).compact();
     }
+
     public Claims getAllClaimsFromToken(String token){
         return parser.parseClaimsJws(token).getBody();
     }
 
+    /**
+     * Получаем имя пользователя из jwt токена
+     * @param token jwt токен
+     * @return имя пользователя, которое было в jwt токене
+     */
     public String getUserName(String token) {
         return getAllClaimsFromToken(token).getSubject();
     }
 
+    /**
+     * Получаем список ролей из jwt токена
+     * @param token jwt токен, из которого будут получены роли
+     * @return список ролей, которые были в jwt токене
+     */
     public List<String> getRoles(String token) {
         return getAllClaimsFromToken(token).get("roles", List.class);
     }
