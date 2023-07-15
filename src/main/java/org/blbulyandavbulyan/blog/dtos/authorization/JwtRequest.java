f4113499e99dc929fe3e4158c6edbaf44cbeb293
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-15 10:19:18 +0400

    javadoc added

--- src/main/java/org/blbulyandavbulyan/blog/dtos/authorization/JwtRequest.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/authorization/JwtRequest.java
@@ -1,4 +1,9 @@
 package org.blbulyandavbulyan.blog.dtos.authorization;
 
+/**
+ * Данная запись предназначена для запроса JWT токена, для авторизации, приходит на сервер в формате JSON
+ * @param username имя пользователя
+ * @param password пароль
+ */
 public record JwtRequest (String username, String password){
 }
