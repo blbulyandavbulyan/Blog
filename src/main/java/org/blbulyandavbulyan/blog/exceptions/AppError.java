445c4fabec755a6ef2206e91516eda7917f969f2
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-15 10:15:41 +0400

    javadoc improved

--- src/main/java/org/blbulyandavbulyan/blog/exceptions/AppError.java
+++ src/main/java/org/blbulyandavbulyan/blog/exceptions/AppError.java
@@ -9,6 +9,11 @@ import java.util.Date;
  * @param timestamp время ошибки
  */
 public record AppError (int status, String message, Date timestamp){
+    /**
+     * Создаёт экземпляр, в качестве timestamp используется текущее время
+     * @param status статус код
+     * @param message сообщение об ошибке
+     */
     public AppError(int status, String message) {
         this(status, message, new Date());
     }
