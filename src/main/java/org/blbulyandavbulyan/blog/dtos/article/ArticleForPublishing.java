2433f78729b453cc7e368cbe9776efb9b0dc5d83
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-15 10:17:04 +0400

    javadoc added

--- src/main/java/org/blbulyandavbulyan/blog/dtos/article/ArticleForPublishing.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/article/ArticleForPublishing.java
@@ -1,4 +1,9 @@
 package org.blbulyandavbulyan.blog.dtos.article;
 
+/**
+ * Данная предназначена для приёма от пользователя статьи для публикации
+ * @param title название статьи
+ * @param text текст статьи
+ */
 public record ArticleForPublishing(String title, String text) {
 }
