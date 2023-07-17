6de0a389bc26a831b3128b807ad5315d4aab6e69
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-17 17:12:36 +0400

    Removed exception handling from ArticlesController

--- src/main/java/org/blbulyandavbulyan/blog/controllers/ArticlesController.java
+++ src/main/java/org/blbulyandavbulyan/blog/controllers/ArticlesController.java
@@ -4,9 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.blbulyandavbulyan.blog.dtos.article.ArticleForPublishing;
 import org.blbulyandavbulyan.blog.dtos.article.ArticleInfoDTO;
 import org.blbulyandavbulyan.blog.dtos.article.ArticlePublished;
-import org.blbulyandavbulyan.blog.exceptions.AppError;
-import org.blbulyandavbulyan.blog.exceptions.ArticleNotFoundException;
-import org.blbulyandavbulyan.blog.exceptions.UserNotFoundException;
 import org.blbulyandavbulyan.blog.services.ArticlesService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -21,34 +18,28 @@ import java.security.Principal;
 @RequestMapping("/api/v1/articles")
 public class ArticlesController {
     private final ArticlesService articlesService;
+
     @GetMapping("/{id}")
-    public ResponseEntity<?> getById(@PathVariable Long id){
-        try {
-            return ResponseEntity.ok(articlesService.getById(id));
-        }
-        catch (ArticleNotFoundException e){
-            return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), e.getMessage()), HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(articlesService.getById(id));
     }
+
     @Secured("ROLE_ADMIN")
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteArticleById(@PathVariable Long id){
-        boolean deleted = articlesService.deleteById(id);
-        return (deleted ? ResponseEntity.ok() : ResponseEntity.notFound()).build();
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteArticleById(@PathVariable Long id) {
+        articlesService.deleteById(id);
     }
+
     @Secured("ROLE_PUBLISHER")
     @PostMapping
-    public ResponseEntity<?> publishArticle(@RequestBody ArticleForPublishing articleForPublishing, Principal principal){
-        try {
-            ArticlePublished articlePublished = articlesService.publishArticle(articleForPublishing, principal.getName());
-            return new ResponseEntity<>(articlePublished, HttpStatus.CREATED);
-        }
-        catch (UserNotFoundException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-        }
+    public ResponseEntity<?> publishArticle(@RequestBody ArticleForPublishing articleForPublishing, Principal principal) {
+        ArticlePublished articlePublished = articlesService.publishArticle(articleForPublishing, principal.getName());
+        return new ResponseEntity<>(articlePublished, HttpStatus.CREATED);
     }
+
     @GetMapping("/info/all")
-    public Page<ArticleInfoDTO> getInfoAboutAllArticles(@RequestParam(defaultValue = "5") Integer pageSize, @RequestParam(defaultValue = "0") Integer pageNumber){
+    public Page<ArticleInfoDTO> getInfoAboutAllArticles(@RequestParam(defaultValue = "5") Integer pageSize, @RequestParam(defaultValue = "0") Integer pageNumber) {
         return articlesService.getInfoAboutAll(pageSize, pageNumber);
     }
 }
