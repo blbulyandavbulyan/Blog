3dd8a2446d7a63808a0b704ff4e8b3056b8e13ac
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-17 18:03:37 +0400

    Javadoc added

--- src/main/java/org/blbulyandavbulyan/blog/controllers/AuthController.java
+++ src/main/java/org/blbulyandavbulyan/blog/controllers/AuthController.java
@@ -16,12 +16,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Контроллер обрабатывающий запросы об авторизации
+ */
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
+    /**
+     * Сервис пользователей
+     */
     private final UserService userService;
+    /**
+     * Ссылка на утилитный класс для управления jwt токенами
+     */
     private final JWTTokenUtils jwtTokenUtils;
+    /**
+     * Ссылка на AuthenticationManager для управления аутентификацией
+     */
     private final AuthenticationManager authenticationManager;
+
+    /**
+     * Метод создаёт токен для пользователя, в случае успешного прохождения авторизации
+     * @param authRequest запрос содержащий логин и пароль
+     * @return ответ, содержащий JWT токен в случае успешной авторизации
+     */
     @PostMapping("/auth")
     public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest){
         try {
