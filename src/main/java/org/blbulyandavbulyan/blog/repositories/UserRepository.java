27297deca4baa90f56e03ae1b625b3f62e6726a8
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-24 14:02:06 +0400

    Added getReferenceByName in UserRepository

--- src/main/java/org/blbulyandavbulyan/blog/repositories/UserRepository.java
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/UserRepository.java
@@ -10,4 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByName(String username);
     boolean existsByName(String username);
+
+    User getReferenceByName(String name);
 }
