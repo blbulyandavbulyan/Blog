9d92335c5842780518615c6b0dbde86efc729952
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-24 14:50:57 +0400

    javadoc added

--- src/main/java/org/blbulyandavbulyan/blog/dtos/comment/CommentDTOForPublishing.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/comment/CommentDTOForPublishing.java
@@ -1,4 +1,9 @@
 package org.blbulyandavbulyan.blog.dtos.comment;
 
+/**
+ * DTO для комментария, отправляемое пользователем в виде JSON для публикации
+ * @param articleId ИД статьи, под которой будет опубликован комментарий
+ * @param text текст комментария
+ */
 public record CommentDTOForPublishing(Long articleId, String text) {
 }
