96c2d27a892b7299d27352dea535ea7490e37d4a
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-24 19:28:21 +0400

    Added RoleService

---
+++ src/main/java/org/blbulyandavbulyan/blog/services/RoleService.java
@@ -0,0 +1,18 @@
+package org.blbulyandavbulyan.blog.services;
+
+import lombok.RequiredArgsConstructor;
+import org.blbulyandavbulyan.blog.entities.Role;
+import org.blbulyandavbulyan.blog.repositories.RoleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoleService {
+    private final RoleRepository roleRepository;
+    public Role getReferenceByRoleName(String roleName){
+        return roleRepository.getReferenceByRoleName(roleName);
+    }
+    public boolean existsByRoleName(String roleName){
+        return roleRepository.existsByRoleName(roleName);
+    }
+}
