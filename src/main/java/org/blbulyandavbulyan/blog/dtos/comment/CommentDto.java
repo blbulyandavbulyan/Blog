89e20abe2aa325b822f313272fc5f1f6c974ed28
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-24 14:50:54 +0400

    javadoc added

--- src/main/java/org/blbulyandavbulyan/blog/dtos/comment/CommentDto.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/comment/CommentDto.java
@@ -2,5 +2,12 @@ package org.blbulyandavbulyan.blog.dtos.comment;
 
 import java.time.ZonedDateTime;
 
+/**
+ * DTO для комментария, содержащее всю необходимую информацию для отображения
+ * @param commentId ИД комментария
+ * @param authorName имя автора
+ * @param text текст комментария
+ * @param publishDate дата публикации
+ */
 public record CommentDto(Long commentId, String authorName, String text, ZonedDateTime publishDate) {
 }
