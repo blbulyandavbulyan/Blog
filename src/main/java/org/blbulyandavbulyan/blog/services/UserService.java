cf45ac981ba00817b74124a456482b5da4d851ba
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-24 19:31:43 +0400

    In UserService added method createUser for administrators

--- src/main/java/org/blbulyandavbulyan/blog/services/UserService.java
+++ src/main/java/org/blbulyandavbulyan/blog/services/UserService.java
@@ -1,7 +1,9 @@
 package org.blbulyandavbulyan.blog.services;
 
 import lombok.RequiredArgsConstructor;
+import org.blbulyandavbulyan.blog.dtos.user.UserCreateRequest;
 import org.blbulyandavbulyan.blog.entities.User;
+import org.blbulyandavbulyan.blog.exceptions.IllegalRoleNameException;
 import org.blbulyandavbulyan.blog.exceptions.users.UserAlreadyExistsException;
 import org.blbulyandavbulyan.blog.exceptions.users.UserNotFoundException;
 import org.blbulyandavbulyan.blog.repositories.UserRepository;
@@ -10,11 +12,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final RoleService roleService;
     private final PasswordEncoder passwordEncoder;
 
     public User registerUser(String userName, String password){
@@ -47,5 +51,14 @@ public class UserService implements UserDetailsService {
 
     public User getReferenceByName(String name) {
         return userRepository.getReferenceByName(name);
+    }
+    @Transactional(rollbackFor = {UserAlreadyExistsException.class, IllegalRoleNameException.class})
+    public void createUser(UserCreateRequest userCreateRequest) {
+        User user = registerUser(userCreateRequest.name(), userCreateRequest.password());//регистрируем пользователя
+        //а теперь устанавливаем ему необходимые привилегии
+        for(String roleName : userCreateRequest.roleNames()){
+            if(!roleService.existsByRoleName(roleName))throw new IllegalRoleNameException("Role with name + " + roleName + " not found!");
+        }
+        user.setRoles(userCreateRequest.roleNames().stream().map(roleService::getReferenceByRoleName).toList());
     }
 }
