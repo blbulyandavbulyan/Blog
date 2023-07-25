4f98cf8c848ab1fa6465c19c07e3804b1f04e62e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-25 21:26:15 +0400

    javadoc for class added

--- src/main/java/org/blbulyandavbulyan/blog/exceptions/users/UserAlreadyExistsException.java
+++ src/main/java/org/blbulyandavbulyan/blog/exceptions/users/UserAlreadyExistsException.java
@@ -2,6 +2,9 @@ package org.blbulyandavbulyan.blog.exceptions.users;
 
 import org.blbulyandavbulyan.blog.exceptions.BlogException;
 
+/**
+ * Данное исключение бросается в случае если на момент регистрации/создания пользователь уже существовал
+ */
 public class UserAlreadyExistsException extends BlogException {
     public UserAlreadyExistsException(String message) {
         super(message);
