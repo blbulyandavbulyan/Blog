9988ccda0f5209639bb02e9c97241316fe7b0751
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-25 21:15:03 +0400

    javadoc added for ArticleInfoDTO

--- src/main/java/org/blbulyandavbulyan/blog/dtos/article/ArticleInfoDTO.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/article/ArticleInfoDTO.java
@@ -1,4 +1,9 @@
 package org.blbulyandavbulyan.blog.dtos.article;
 
+/**
+ * DTO для представления базовой информации о статье, для отправки клиенту
+ * @param articleId ИД статьи
+ * @param title название статьи
+ */
 public record ArticleInfoDTO(Long articleId, String title) {
 }
