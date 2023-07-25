0475cb4fa9d98dccd7ef1fb0e30542c2e92d7abf
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-25 21:28:01 +0400

    javadoc for CommentController added

--- src/main/java/org/blbulyandavbulyan/blog/controllers/rest/CommentController.java
+++ src/main/java/org/blbulyandavbulyan/blog/controllers/rest/CommentController.java
@@ -11,15 +11,35 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
+/**
+ * Контроллер для работы с комментариями
+ */
 @RestController
 @RequestMapping("/api/v1/comments")
 @RequiredArgsConstructor
 public class CommentController {
+    /**
+     * Сервис для работы с комментариями
+     */
     private final CommentService commentService;
+
+    /**
+     * Получает все комментарии к заданной статье в виде страницы
+     * @param articleId ИД страницы
+     * @param pageNumber номер страницы(начиная с 1)
+     * @param pageSize размер страницы
+     * @return страницу, содержащую комментарии к заданной статье
+     */
     @GetMapping("/article/{articleId}")
     public Page<CommentDto> getAllCommentsForArticle(@PathVariable Long articleId, @RequestParam(name = "p", defaultValue = "1") Integer pageNumber, @RequestParam(name = "s", defaultValue = "10") Integer pageSize){
         return commentService.getCommentDTOsForArticleId(articleId, pageNumber - 1, pageSize);
     }
+
+    /**
+     * Публикует полученный комментарий
+     * @param commentForPublishing DTO комментария, который нужно опубликовать
+     * @param principal ссылка на principal, в котором будет имя авторизованного пользователя
+     */
     @Secured({"ROLE_COMMENTER"})
     @PostMapping("/article")
     @ResponseStatus(HttpStatus.CREATED)
