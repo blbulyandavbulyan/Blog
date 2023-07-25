65d51553ff759bd524ea2e3578be2aece2741f98
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-25 20:33:33 +0400

    Added method for returning dto by id in UserRepository

--- src/main/java/org/blbulyandavbulyan/blog/repositories/UserRepository.java
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByName(String username);
 
     User getReferenceByName(String name);
+
+    <T> T findByUserId(Long userId, Class<T> dtoClass);
 }
