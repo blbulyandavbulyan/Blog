2c90898b9a8ebb00d7a83b80615e27a8fd4500a7
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-25 21:26:21 +0400

    javadoc for class added

--- src/main/java/org/blbulyandavbulyan/blog/exceptions/users/UserNotFoundException.java
+++ src/main/java/org/blbulyandavbulyan/blog/exceptions/users/UserNotFoundException.java
@@ -2,6 +2,9 @@ package org.blbulyandavbulyan.blog.exceptions.users;
 
 import org.blbulyandavbulyan.blog.exceptions.ResourceNofFoundException;
 
+/**
+ * Данное исключение бросается, в случае если пользователь не был найден по ИД или имени
+ */
 public class UserNotFoundException extends ResourceNofFoundException {
     public UserNotFoundException(String message) {
         super(message);
