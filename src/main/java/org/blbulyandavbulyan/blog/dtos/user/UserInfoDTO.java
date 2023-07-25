04f371c87a8d8893f91b6793405eaee7acefce4c
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-25 20:36:37 +0400

    javadoc for UserInfoDTO added

--- src/main/java/org/blbulyandavbulyan/blog/dtos/user/UserInfoDTO.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/user/UserInfoDTO.java
@@ -2,14 +2,39 @@ package org.blbulyandavbulyan.blog.dtos.user;
 
 import java.util.List;
 
+/**
+ * Данный интерфейс предоставляет DTO для информации о пользователе, получаемой администратором
+ */
 public interface UserInfoDTO {
+    /**
+     * Получает ИД пользователя
+     * @return ИД пользователя
+     */
 
     Long getUserId();
 
+    /**
+     * Получает имя пользователя
+     * @return имя пользователя
+     */
+
     String getName();
 
+    /**
+     * Получает спиcок ролей данного пользователя
+     * @return  список ролей пользователя
+     */
+
     List<RoleDto> getRoles();
+
+    /**
+     * DTO для роли пользователя
+     */
     interface RoleDto{
+        /**
+         * Получает имя данной роли
+         * @return имя данной роли
+         */
         String getName();
     }
 
