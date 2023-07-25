a8ec24cfa84b403ebbcedca14a65e01e08bc898e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-25 21:28:14 +0400

    javadoc for class added

--- src/main/java/org/blbulyandavbulyan/blog/exceptions/articles/ArticleNotFoundException.java
+++ src/main/java/org/blbulyandavbulyan/blog/exceptions/articles/ArticleNotFoundException.java
@@ -2,6 +2,9 @@ package org.blbulyandavbulyan.blog.exceptions.articles;
 
 import org.blbulyandavbulyan.blog.exceptions.ResourceNofFoundException;
 
+/**
+ * Данное исключение бросается, в случае если статья по заданному ИД не была найдена
+ */
 public class ArticleNotFoundException extends ResourceNofFoundException {
     public ArticleNotFoundException(String message) {
         super(message);
