dd61c8d8bc816a1b3d34124c675d67287f29cffc
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-27 17:42:12 +0400

    removed unused imports

--- src/main/java/org/blbulyandavbulyan/blog/configs/PersistenceConfiguration.java
+++ src/main/java/org/blbulyandavbulyan/blog/configs/PersistenceConfiguration.java
@@ -1,8 +1,6 @@
 package org.blbulyandavbulyan.blog.configs;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
