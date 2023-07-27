31286b2bb6ac836708e434960e4a274d213bf63a
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-27 11:17:21 +0400

    Removed useless config

--- src/main/java/org/blbulyandavbulyan/blog/configs/GeneralConfig.java
+++
@@ -1,9 +0,0 @@
-package org.blbulyandavbulyan.blog.configs;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("org.blbuluyandavbulyan.blog")
-public class GeneralConfig {
-}
