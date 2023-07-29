51cd611df5166330deb47f08ac20258ed152fa19
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-29 15:44:47 +0400

    Added controller for showing articles pages

---
+++ src/main/java/org/blbulyandavbulyan/blog/controllers/ArticleTemplatesController.java
@@ -0,0 +1,21 @@
+package org.blbulyandavbulyan.blog.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.blbulyandavbulyan.blog.services.ArticlesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/articles")
+@RequiredArgsConstructor
+public class ArticleTemplatesController {
+    private final ArticlesService articlesService;
+    @GetMapping("/{id}")
+    public String showArticleById(@PathVariable Long id, Model model){
+        model.addAttribute("article", articlesService.getById(id));
+        return "article";
+    }
+}
