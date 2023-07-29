1e92a4cb4716f4df38c5507e74a95047b93b0540
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-29 17:11:05 +0400

    added auth.js

---
+++ src/main/resources/static/auth.js
@@ -0,0 +1,27 @@
+//данный файл содержит необходимый код для авторизации
+// Определение контроллера AuthController
+app.controller('AuthController', function($scope, $http) {
+  $scope.credentials = {
+    username: '',
+    password: ''
+  };
+
+  $scope.token = null;
+  $scope.error = null;
+
+  $scope.login = function() {
+    $scope.error = null; // Сбрасываем предыдущую ошибку
+
+    // Предполагаем, что на сервере у вас есть маршрут для аутентификации и получения токена
+    // Здесь отправляем POST-запрос с данными авторизации
+    $http.post('/api/login', $scope.credentials)
+      .then(function(response) {
+        // В ответе сервера должен быть токен, который вы сохраняете в переменной $scope.token
+        $scope.token = response.data.token;
+      })
+      .catch(function(error) {
+        console.error('Ошибка авторизации:', error);
+        $scope.error = 'Ошибка авторизации. Пожалуйста, проверьте правильность введенных данных.';
+      });
+  };
+});
