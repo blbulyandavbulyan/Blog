148caff0382ce07e965c8ddaa66e28b7bd01d3e1
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-29 18:50:28 +0400

    moved function for validating tokens in AuthController

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -8,7 +8,19 @@ app.controller('AuthController', function($scope, $http) {
 
   $scope.token = null;
   $scope.error = null;
+  $scope.isValidToken = function () {
+       const token = getCookie('token');
+       if (!token) return false; // Если токен не существует, считаем его недействительным
 
+       const payloadBase64 = token.split('.')[1];
+       const payloadJson = atob(payloadBase64);
+       const payload = JSON.parse(payloadJson);
+
+       // Получить текущее время в секундах (Unix timestamp)
+       const currentTime = Math.floor(Date.now() / 1000);
+
+       return payload.exp > currentTime; // Если время истечения токена больше текущего времени, то считаем его действительным
+  };
   $scope.login = function() {
     $scope.error = null; // Сбрасываем предыдущую ошибку
 
@@ -35,16 +47,3 @@ app.controller('AuthController', function($scope, $http) {
       });
   };
 });
-function isTokenValid() {
-     const token = getCookie('token');
-     if (!token) return false; // Если токен не существует, считаем его недействительным
-
-     const payloadBase64 = token.split('.')[1];
-     const payloadJson = atob(payloadBase64);
-     const payload = JSON.parse(payloadJson);
-
-     // Получить текущее время в секундах (Unix timestamp)
-     const currentTime = Math.floor(Date.now() / 1000);
-
-     return payload.exp > currentTime; // Если время истечения токена больше текущего времени, то считаем его действительным
-}
