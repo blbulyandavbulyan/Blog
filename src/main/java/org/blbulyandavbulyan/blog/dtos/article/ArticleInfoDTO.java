256822fcc1bbb96064bfd42822cb3e7ee2733cde
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-30 15:52:04 +0400

    publishDate and publisherName fields added in ArticleInfoDTO

--- src/main/java/org/blbulyandavbulyan/blog/dtos/article/ArticleInfoDTO.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/article/ArticleInfoDTO.java
@@ -1,9 +1,13 @@
 package org.blbulyandavbulyan.blog.dtos.article;
 
+import java.time.ZonedDateTime;
+
 /**
  * DTO для представления базовой информации о статье, для отправки клиенту
  * @param articleId ИД статьи
+ * @param publisherName имя автора статьи
+ * @param publishDate дата публикации
  * @param title название статьи
  */
-public record ArticleInfoDTO(Long articleId, String title) {
+public record ArticleInfoDTO(Long articleId, String publisherName, ZonedDateTime publishDate, String title) {
 }
