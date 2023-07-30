217ed88a32ecca9201cc76bfddb490b683d5ea3d
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-30 16:21:39 +0400

    added calculatePageNumbers to app.js, because this function will be useful in other controllers

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,28 @@
 var app = angular.module('blog', []);
 // Определение интерцептора
 const contextPath = window.location.origin + '/blog';
+function calculatePageNumbers(currentPage, totalPages, maxPagesToShow) {
+  var pageNumbers = [];
+  if (totalPages <= maxPagesToShow) {
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
+    }
+  } else {
+    let startPage;
+    let endPage;
+    if (currentPage <= Math.ceil(maxPagesToShow / 2)) {
+      startPage = 1;
+      endPage = maxPagesToShow;
+    } else if (currentPage >= totalPages - Math.floor(maxPagesToShow / 2)) {
+      startPage = totalPages - maxPagesToShow + 1;
+      endPage = totalPages;
+    } else {
+      startPage = currentPage - Math.floor(maxPagesToShow / 2);
+      endPage = currentPage + Math.floor(maxPagesToShow / 2);
+    }
+    for (let i = startPage; i <= endPage; i++) {
+      pageNumbers.push(i);
+    }
+  }
+  return pageNumbers;
+}
