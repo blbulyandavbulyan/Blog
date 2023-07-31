e03ffe0614cd7fbe8561e4c61a17b5a8b7a6cd03
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-31 20:55:52 +0400

    added implementation of JpaSpecificationExecutor

--- src/main/java/org/blbulyandavbulyan/blog/repositories/ArticleRepository.java
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/ArticleRepository.java
@@ -4,13 +4,14 @@ import org.blbulyandavbulyan.blog.entities.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-//    @Query("SELECT new org.blbulyandavbulyan.blog.dtos.article.ArticleDto(a.title, a.text, a.publisher.name) FROM #{#entityName} a WHERE a.id = :articleId")
+public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
+
     <T> Optional<T> findByArticleId(Long articleId, Class<T> dtoType);
 
     <T> Page<T> findAllPagesBy(Class<T> type, final Pageable pageable);
