1dd24d4dfe92840b07e451d66be3918d4fb84563
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-31 20:53:26 +0400

    Added ArticleSpecification

---
+++ src/main/java/org/blbulyandavbulyan/blog/specs/ArticleSpecification.java
@@ -0,0 +1,39 @@
+package org.blbulyandavbulyan.blog.specs;
+
+import lombok.Getter;
+import org.blbulyandavbulyan.blog.entities.Article;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+@Getter
+public class ArticleSpecification {
+    private Specification<Article> articleSpecification;
+    public ArticleSpecification(Map<String, String> filterParams){
+        articleSpecification = Specification.where(null);
+        for(var entries : filterParams.entrySet()){
+            if(entries.getKey().equals("maxDate")){
+                articleSpecification = articleSpecification.and(untilDate(entries.getValue()));
+            }
+            else if(entries.getKey().equals("author")){
+                articleSpecification = articleSpecification.and(authorLike(entries.getValue()));
+            }
+            else if(entries.getKey().equals("title")){
+                articleSpecification = articleSpecification.and(titleLike(entries.getValue()));
+            }
+        }
+    }
+
+    public static Specification<Article> titleLike(String title) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), "%%%s%%".formatted(title));
+    }
+
+    public static Specification<Article> authorLike(String author){
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("publisher").get("name"), "%%%s%%".formatted(author));
+    }
+    public static Specification<Article> untilDate(String date){
+        return (root, query, criteriaBuilder)-> criteriaBuilder.lessThanOrEqualTo(root.get("publishDate"), ZonedDateTime.parse(date,   DateTimeFormatter.ISO_DATE_TIME));
+    }
+}
