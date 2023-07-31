2e646d57b95f8d39dd1614a580ef77105aa88973
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-31 21:01:10 +0400

    added additional map in request params, it needs for create specification. Also added creation of specification in getInfoABoutAllArticles

--- src/main/java/org/blbulyandavbulyan/blog/controllers/rest/ArticlesController.java
+++ src/main/java/org/blbulyandavbulyan/blog/controllers/rest/ArticlesController.java
@@ -6,12 +6,14 @@ import org.blbulyandavbulyan.blog.dtos.article.ArticleForPublishing;
 import org.blbulyandavbulyan.blog.dtos.article.ArticleInfoDTO;
 import org.blbulyandavbulyan.blog.dtos.article.ArticlePublished;
 import org.blbulyandavbulyan.blog.services.ArticlesService;
+import org.blbulyandavbulyan.blog.specs.ArticleSpecification;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Map;
 
 /**
  * Контроллер для статей
@@ -66,7 +68,7 @@ public class ArticlesController {
      * @return найденную страницу с краткой информацией о статьях
      */
     @GetMapping("/info/all")
-    public Page<ArticleInfoDTO> getInfoAboutAllArticles(@RequestParam(defaultValue = "5", name = "s") Integer pageSize, @RequestParam(defaultValue = "1", name = "p") Integer pageNumber) {
-        return articlesService.getInfoAboutAll(pageSize, pageNumber - 1);
+    public Page<ArticleInfoDTO> getInfoAboutAllArticles(@RequestParam(defaultValue = "5", name = "s") Integer pageSize, @RequestParam(defaultValue = "1", name = "p") Integer pageNumber, @RequestParam Map<String, String> requestParams) {
+        return articlesService.getInfoAboutAll(new ArticleSpecification(requestParams).getArticleSpecification(), pageSize, pageNumber - 1);
     }
 }
