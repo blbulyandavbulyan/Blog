04f6a17a4278dc53c98d7d5221ded0243106f35f
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-01 14:07:34 +0400

    added UserController

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -40,3 +40,33 @@ app.service('UserService', function($http){
         }
     }
 });
+app.controller('UserController', function($scope, UserService){
+    $scope.users = [];
+    $scope.filterParams = {};
+    $scope.filter = {};
+    $scope.currentPage = 1;
+    $scope.itemsPerPage = 5;
+    $scope.totalPages = 5;
+    const maxPagesToShow = 3; // Максимальное количество отображаемых страниц
+    $scope.filterArticles = function(){
+         $scope.filterParams = $scope.filter;
+         $scope.getPage(1);
+    }
+    $scope.loadUsersInfo = function(filterParams, pageNumber) {
+      UserService.getUserInfoAboutAllUsers(filterParams, pageNumber, $scope.itemsPerPage)
+        .then(function(response) {
+          $scope.users = response.data.content;
+          $scope.totalPages = response.data.totalPages;
+          $scope.currentPage = pageNumber;
+        });
+    };
+    $scope.getPage = function(pageNumber){
+         $scope.loadUsersInfo($scope.filterParams, pageNumber);
+         $scope.pageNumbers = calculatePageNumbers($scope.currentPage, $scope.totalPages, maxPagesToShow);
+    }
+    // Обработчик изменения общего количества страниц (возможно, при загрузке данных с сервера)
+    $scope.$watch('totalPages', function() {
+      $scope.pageNumbers = calculatePageNumbers($scope.currentPage, $scope.totalPages, maxPagesToShow);
+    });
+    $scope.getPage(1);
+});
