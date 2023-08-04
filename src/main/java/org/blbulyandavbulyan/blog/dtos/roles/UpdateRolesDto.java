6e9a5968e7162ab2b14d077105b95543b33d09ae
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-04 17:49:36 +0400

    javadoc added

--- src/main/java/org/blbulyandavbulyan/blog/dtos/roles/UpdateRolesDto.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/roles/UpdateRolesDto.java
@@ -3,5 +3,10 @@ package org.blbulyandavbulyan.blog.dtos.roles;
 
 import java.util.List;
 
+/**
+ * Запрос на обновления ролей у пользователя
+ * @param userId ИД пользователя, роли которого нужно обновить
+ * @param rolesNames список ролей, которые будут вместо старых
+ */
 public record UpdateRolesDto(Long userId, List<String> rolesNames) {
 }
