1aa47daca0c4eb30eb32c2175c01bda1b0dca7a3
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-11 14:38:30 +0400

    In navbar.js NavbarController added isActive method, in navbar.html added new item for user_control, also added ng-class attribute for nav links

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,4 +1,7 @@
-app.controller('NavbarController', function($scope, RoleService){
+app.controller('NavbarController', function($scope, $location, RoleService){
     $scope.showPostArticleItem = ()=> RoleService.isPublisher();
     $scope.canAdministrate = ()=> RoleService.isAdmin();
+    $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
 });
