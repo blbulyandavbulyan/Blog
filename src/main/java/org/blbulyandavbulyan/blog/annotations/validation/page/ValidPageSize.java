00ce78724c02a9f7ff4dbdae2c53181c347e2fb4
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-12 18:34:26 +0400

    Added validation annotations for page request, ValidPageNumber and ValidPageSize

---
+++ src/main/java/org/blbulyandavbulyan/blog/annotations/validation/page/ValidPageSize.java
@@ -0,0 +1,26 @@
+package org.blbulyandavbulyan.blog.annotations.validation.page;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.RECORD_COMPONENT, ElementType.PARAMETER, ElementType.FIELD})
+@Constraint(validatedBy = {})
+@NotNull
+@Min(5)
+@Max(100)
+public @interface ValidPageSize {
+    String message() default "Invalid page size";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
