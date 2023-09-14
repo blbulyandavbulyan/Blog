98a38d6e7952b53a4c4f404008eefe66cd5c1165
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-15 00:13:53 +0400

    added ValidCommentId annotation, for validation comment id

---
+++ src/main/java/org/blbulyandavbulyan/blog/annotations/validation/comment/ValidCommentId.java
@@ -0,0 +1,26 @@
+package org.blbulyandavbulyan.blog.annotations.validation.comment;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+/**Данная аннотация предназначена для валидации ID комментария*/
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.RECORD_COMPONENT, ElementType.PARAMETER, ElementType.FIELD})
+@Constraint(validatedBy = {})
+@NotNull
+@Min(1)
+@Max(Long.MAX_VALUE)
+public @interface ValidCommentId {
+    String message() default "Invalid article id";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
