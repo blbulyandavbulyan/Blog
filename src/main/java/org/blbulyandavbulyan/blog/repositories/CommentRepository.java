d7a9df9d32934c8ceacdcb95b4a224a4fdb00cc5
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-15 10:18:06 +0400

    added CommentRepository#find_AuthorNameByCommentId

--- src/main/java/org/blbulyandavbulyan/blog/repositories/CommentRepository.java
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/CommentRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     <T> Page<T> findAllByArticleArticleId(Long id, Pageable pageable, Class<T> dtoType);
+    Optional<String> find_AuthorNameByCommentId(Long commentId);
 }
