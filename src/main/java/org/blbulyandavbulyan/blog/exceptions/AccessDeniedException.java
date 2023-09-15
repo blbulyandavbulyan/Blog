9a07ddfddb8f60d95e4ce9950deb8b8412d07427
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-15 11:56:58 +0400

    added AccessDeniedException and CommentNotFoundException

---
+++ src/main/java/org/blbulyandavbulyan/blog/exceptions/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package org.blbulyandavbulyan.blog.exceptions;
+
+public class AccessDeniedException extends BlogException{
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
