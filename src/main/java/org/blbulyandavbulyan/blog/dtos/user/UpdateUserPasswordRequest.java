9d1eb98f70bee1df59f91cc8a5f74bf70c1f6fcd
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-15 17:52:00 +0400

    added validation annotations for UpdateUserPasswordRequest

--- src/main/java/org/blbulyandavbulyan/blog/dtos/user/UpdateUserPasswordRequest.java
+++ src/main/java/org/blbulyandavbulyan/blog/dtos/user/UpdateUserPasswordRequest.java
@@ -1,9 +1,12 @@
 package org.blbulyandavbulyan.blog.dtos.user;
 
+import org.blbulyandavbulyan.blog.annotations.validation.user.ValidRawPassword;
+import org.blbulyandavbulyan.blog.annotations.validation.user.ValidUsername;
+
 /**
  * Данный класс предоставляет DTO для обновления пароля пользователя
  * @param username имя пользователя, для которого будет обновлён пароль
  * @param password новый пароль
  */
-public record UpdateUserPasswordRequest(String username, String password) {
+public record UpdateUserPasswordRequest(@ValidUsername String username, @ValidRawPassword String password) {
 }
