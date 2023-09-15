47d489f079e5f09fb5fe46727490fdbbba193694
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-15 15:52:44 +0400

    Added UserRepository#updatePasswordHashByName

--- src/main/java/org/blbulyandavbulyan/blog/repositories/UserRepository.java
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/UserRepository.java
@@ -3,12 +3,19 @@ package org.blbulyandavbulyan.blog.repositories;
 import org.blbulyandavbulyan.blog.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    @Transactional
+    @Modifying
+    @Query("update User u set u.passwordHash = ?1 where u.name = ?2")
+    int updatePasswordHashByName(String passwordHash, String name);
     Optional<User> findByName(String username);
     boolean existsByName(String username);
 
