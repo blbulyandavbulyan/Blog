fa3774ae4020588075a913e173a9cb48c3e49ccf
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-16 17:47:49 +0400

    added overriding CommentRepository#deleteById method, because standard method does unnecessary select

--- src/main/java/org/blbulyandavbulyan/blog/repositories/CommentRepository.java
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/CommentRepository.java
@@ -4,8 +4,10 @@ import org.blbulyandavbulyan.blog.entities.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,4 +17,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     <T> Page<T> findAllByArticleArticleId(Long id, Pageable pageable, Class<T> dtoType);
     @Query("SELECT c.author.name FROM Comment c WHERE c.commentId = :commentId")
     Optional<String> findCommentAuthorNameByCommentId(@Param("commentId") Long commentId);
+
+    @Override
+    @Modifying
+    @Query("DELETE FROM Comment WHERE commentId = :id")
+    void deleteById(@NonNull @Param("id") Long aLong);
 }
