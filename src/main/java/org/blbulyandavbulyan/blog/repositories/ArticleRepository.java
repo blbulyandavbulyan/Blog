a447d97fd3fc64ea28d2dba5fb45a3b9fee6fc4a
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-16 17:57:48 +0400

    Added overriding ArticleRepository#deleteById, because standard method generates useless and unnecessary select query

--- src/main/java/org/blbulyandavbulyan/blog/repositories/ArticleRepository.java
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/ArticleRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -19,4 +21,9 @@ public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpec
     <T> Page<T> findAllPagesBy(Class<T> type, final Pageable pageable);
     @Query("SELECT a.publisher.name FROM Article a WHERE a.articleId = :id")
     Optional<String> findArticleAuthorNameByArticleId(@Param("id") Long id);
+
+    @Override
+    @Modifying
+    @Query("DELETE FROM Article a WHERE a.articleId = :id")
+    void deleteById(@NonNull @Param("id") Long articleId);
 }
