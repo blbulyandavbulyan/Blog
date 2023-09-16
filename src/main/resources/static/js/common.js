afcf2cb273506272191b94ddb988a5a57f7b7272
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-16 13:09:57 +0400

    Moved global common functions to common.js from app.js, added including common.js in index.html file

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,52 @@
+function calculatePageNumbers(currentPage, totalPages, maxPagesToShow) {
+    const pageNumbers = [];
+    if (totalPages <= maxPagesToShow) {
+        for (let i = 1; i <= totalPages; i++) {
+            pageNumbers.push(i);
+        }
+    } else {
+        let startPage;
+        let endPage;
+        if (currentPage <= Math.ceil(maxPagesToShow / 2)) {
+            startPage = 1;
+            endPage = maxPagesToShow;
+        } else if (currentPage >= totalPages - Math.floor(maxPagesToShow / 2)) {
+            startPage = totalPages - maxPagesToShow + 1;
+            endPage = totalPages;
+        } else {
+            startPage = currentPage - Math.floor(maxPagesToShow / 2);
+            endPage = currentPage + Math.floor(maxPagesToShow / 2);
+        }
+        for (let i = startPage; i <= endPage; i++) {
+            pageNumbers.push(i);
+        }
+    }
+    return pageNumbers;
+}
+
+function generatePageParamsAndFilterParams(filterParams, pageNumber, pageSize) {
+    const httpParams = {
+        p: pageNumber,
+        s: pageSize
+    };
+    Object.keys(filterParams).forEach(function (key) {
+        let value = filterParams[key];
+        if (value === 'string') {
+            value = value.trim();
+        }
+        if (value !== '') {
+            httpParams[key] = value;
+        }
+    });
+    return httpParams;
+}
+function showErrorToast(caption, text){
+    const errorToast = document.getElementById('errorToast');
+    errorToast.getElementsByClassName("error-text")[0].textContent = text;
+    errorToast.getElementsByClassName("error-caption")[0].textContent = caption;
+    bootstrap.Toast.getOrCreateInstance(errorToast).show();
+}
+function showChangePasswordDialog(){
+    const myModalAlternative = new bootstrap.Modal('#changePasswordModal', {});
+    myModalAlternative.show();
+}
