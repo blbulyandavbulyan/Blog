18349d5335f5ca4c7c931c6269f5fa992b1914f8
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-21 23:19:58 +0400

    added request processing indicator for edit comment

--- src/main/resources/static/js/comments.js
+++ src/main/resources/static/js/comments.js
@@ -48,6 +48,7 @@ app.controller('CommentController', function ($scope, $routeParams, $timeout, Co
     $scope.contentLoading = false;
     $scope.loadingError = null;
     $scope.postCommentRequestProcessed = false;
+    $scope.editCommentRequestProcessed = false;
     $scope.newComment = {
         text: ''
     };
@@ -113,21 +114,29 @@ app.controller('CommentController', function ($scope, $routeParams, $timeout, Co
     }
     $scope.editItem = function (comment){
         const modalDialogElement = document.getElementById("editCommentModal");
+        const editCommentDialog = new bootstrap.Modal(modalDialogElement, {});
         document.getElementById("confirmCommentEdit").onclick = function () {
+            $scope.editCommentRequestProcessed = true;
             const text = $scope.editedComment.text;
             CommentService.editComment(comment.commentId, text)
-                .then(function () {
-                    const index = $scope.comments.findIndex(c=>c.commentId === comment.commentId);
-                    if(index !== -1) $scope.comments[index].text = text;
-                })
+                .then(() =>
+                    $timeout(function () {
+                        const index = $scope.comments.findIndex(c => c.commentId === comment.commentId);
+                        if (index !== -1) $scope.comments[index].text = text;
+                        $scope.editCommentRequestProcessed = false;
+                        editCommentDialog.hide();
+                    }, 300)
+                )
                 .catch(function(error){
-                    showErrorToast("Ошибка редактирования", "Не удалось отредактировать комментарий")
-                    console.log(error);
+                    $timeout(function (){
+                        showErrorToast("Ошибка редактирования", "Не удалось отредактировать комментарий")
+                        console.log(error);
+                        $scope.editCommentRequestProcessed = false;
+                    }, 300);
                 })
         }
         $scope.editedComment.text = comment.text;
         $scope.charactersLeftForEditedComment = $scope.maxCommentLength - comment.text;
-        const editCommentDialog = new bootstrap.Modal(modalDialogElement, {});
         editCommentDialog.show();
     }
     $scope.canPost = RoleService.isCommenter;
