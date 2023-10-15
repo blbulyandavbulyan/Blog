a0cd2eaf1b5c834ca3210ac7e44634ce6ece0508
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-15 17:46:07 +0400

    added ArticleReactionController

---
+++ src/main/java/org/blbulyandavbulyan/blog/controllers/rest/ArticleReactionController.java
@@ -0,0 +1,26 @@
+package org.blbulyandavbulyan.blog.controllers.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.blbulyandavbulyan.blog.annotations.validation.article.ValidArticleId;
+import org.blbulyandavbulyan.blog.dtos.reactions.ArticleReactionDTO;
+import org.blbulyandavbulyan.blog.services.ArticleReactionService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/reactions/article")
+@RequiredArgsConstructor
+@Validated
+public class ArticleReactionController {
+    private final ArticleReactionService articleReactionService;
+    @PostMapping
+    public void createOrAlterReaction(@RequestBody @Validated ArticleReactionDTO articleReactionDTO, Principal principal){
+        articleReactionService.react(articleReactionDTO.articleId(), principal.getName(), articleReactionDTO.liked());
+    }
+    @DeleteMapping("/{articleId}")
+    public void removeReaction(@PathVariable @ValidArticleId Long articleId, Principal principal){
+        articleReactionService.removeReaction(articleId, principal.getName());
+    }
+}
