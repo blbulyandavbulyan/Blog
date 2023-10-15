b3c03a3304801a890c0027317161bd02dee9dd15
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-15 18:11:08 +0400

    added additional constructor in CommentReaction

--- src/main/java/org/blbulyandavbulyan/blog/entities/CommentReaction.java
+++ src/main/java/org/blbulyandavbulyan/blog/entities/CommentReaction.java
@@ -2,12 +2,14 @@ package org.blbulyandavbulyan.blog.entities;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "comments_reactions")
 @Getter
 @Setter
+@NoArgsConstructor
 public class CommentReaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +23,9 @@ public class CommentReaction {
     private User liker;
     @Column(name = "liked", nullable = false)
     private boolean liked;
+
+    public CommentReaction(Comment comment, User liker) {
+        this.comment = comment;
+        this.liker = liker;
+    }
 }
