2340c7951987fd19fc60975836b9d225d53f0128
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-16 16:07:07 +0400

    now CommentReactionService extends AbstractReactionService

--- src/main/java/org/blbulyandavbulyan/blog/services/reactions/CommentReactionService.java
+++ src/main/java/org/blbulyandavbulyan/blog/services/reactions/CommentReactionService.java
@@ -1,34 +1,15 @@
 package org.blbulyandavbulyan.blog.services.reactions;
 
-import lombok.RequiredArgsConstructor;
 import org.blbulyandavbulyan.blog.entities.Comment;
 import org.blbulyandavbulyan.blog.entities.CommentReaction;
-import org.blbulyandavbulyan.blog.entities.User;
-import org.blbulyandavbulyan.blog.repositories.CommentReactionRepository;
+import org.blbulyandavbulyan.blog.repositories.reactions.CommentReactionRepository;
 import org.blbulyandavbulyan.blog.services.CommentService;
 import org.blbulyandavbulyan.blog.services.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
-public class CommentReactionService {
-    private final UserService userService;
-    private final CommentService commentService;
-    private final CommentReactionRepository commentReactionRepository;
-    /**
-     * Данный метод удаляет реакцию от данного пользователя к данной статье
-     * @param id id статьи, с которой нужно убрать реакцию
-     * @param username имя пользователя, от которого нужно убрать реакцию
-     */
-    public void removeReaction(Long id, String username){
-        commentReactionRepository.deleteByCommentAndLiker(commentService.getReferenceById(id), userService.getReferenceByName(username));
-    }
-    public void react(Long id, String username, boolean liked){
-        Comment comment = commentService.getReferenceById(id);
-        User liker = userService.getReferenceByName(username);
-        CommentReaction reaction = commentReactionRepository.findByCommentAndLiker(comment, liker)
-                .orElseGet(() -> new CommentReaction(comment, liker));
-        reaction.setLiked(liked);
-        commentReactionRepository.save(reaction);
+public class CommentReactionService extends AbstractReactionService<CommentReaction, Comment, CommentReactionRepository>{
+    public CommentReactionService(UserService userService, CommentService commentService, CommentReactionRepository repository) {
+        super(userService, repository, CommentReaction::new, commentService::getReferenceById);
     }
 }
