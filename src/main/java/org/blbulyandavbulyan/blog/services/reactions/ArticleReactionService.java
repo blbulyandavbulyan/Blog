84edfc174ef85e7b00e76654a53322a3a0353f27
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-16 16:06:56 +0400

    now ArticleReactionService extends AbstractReactionService

--- src/main/java/org/blbulyandavbulyan/blog/services/reactions/ArticleReactionService.java
+++ src/main/java/org/blbulyandavbulyan/blog/services/reactions/ArticleReactionService.java
@@ -1,34 +1,15 @@
 package org.blbulyandavbulyan.blog.services.reactions;
 
-import lombok.RequiredArgsConstructor;
 import org.blbulyandavbulyan.blog.entities.Article;
 import org.blbulyandavbulyan.blog.entities.ArticleReaction;
-import org.blbulyandavbulyan.blog.entities.User;
-import org.blbulyandavbulyan.blog.repositories.ArticleReactionRepository;
+import org.blbulyandavbulyan.blog.repositories.reactions.ArticleReactionRepository;
 import org.blbulyandavbulyan.blog.services.ArticleService;
 import org.blbulyandavbulyan.blog.services.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
-public class ArticleReactionService {
-    private final ArticleService articleService;
-    private final ArticleReactionRepository articleReactionRepository;
-    private final UserService userService;
-    /**
-     * Данный метод удаляет реакцию от данного пользователя к данной статье
-     * @param id id статьи, с которой нужно убрать реакцию
-     * @param username имя пользователя, от которого нужно убрать реакцию
-     */
-    public void removeReaction(Long id, String username){
-        articleReactionRepository.deleteByArticleAndLiker(articleService.getReferenceById(id), userService.getReferenceByName(username));
-    }
-    public void react(Long id, String username, boolean liked){
-        Article article = articleService.getReferenceById(id);
-        User liker = userService.getReferenceByName(username);
-        ArticleReaction reaction = articleReactionRepository.findByArticleAndLiker(article, liker)
-                .orElseGet(() -> new ArticleReaction(article, liker));
-        reaction.setLiked(liked);
-        articleReactionRepository.save(reaction);
+public class ArticleReactionService extends AbstractReactionService<ArticleReaction, Article, ArticleReactionRepository>{
+    public ArticleReactionService(UserService userService, ArticleService articleService, ArticleReactionRepository repository) {
+        super(userService, repository, ArticleReaction::new, articleService::getReferenceById);
     }
 }
