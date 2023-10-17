030bf9c9be8cf93f7b4f3ad85d3334f1fa3b6eec
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-17 18:03:04 +0400

    added CommentReactionRepositoryTest

---
+++ src/test/java/org/blbulyandavbulyan/blog/repositories/reactions/CommentReactionRepositoryTest.java
@@ -0,0 +1,10 @@
+package org.blbulyandavbulyan.blog.repositories.reactions;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+@DataJpaTest
+class CommentReactionRepositoryTest {
+    @Autowired
+    private CommentReactionRepository underTest;
+    // TODO: 17.10.2023 добавить сюда методы для тестирования
+}
