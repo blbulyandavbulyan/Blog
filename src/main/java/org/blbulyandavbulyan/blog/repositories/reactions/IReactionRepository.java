ecb42f1bc193c2261666381015fb75baf587ea92
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-17 13:14:45 +0400

    added IReactionRepository#getStatistics method with jpql query, to count likes and dislikes

--- src/main/java/org/blbulyandavbulyan/blog/repositories/reactions/IReactionRepository.java
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/reactions/IReactionRepository.java
@@ -1,8 +1,11 @@
 package org.blbulyandavbulyan.blog.repositories.reactions;
 
+import org.blbulyandavbulyan.blog.dtos.reactions.ReactionStatistics;
 import org.blbulyandavbulyan.blog.entities.User;
 import org.blbulyandavbulyan.blog.entities.reactions.IReaction;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -17,5 +20,17 @@ public interface IReactionRepository<RT extends IReaction, TT> {
     @Modifying
     @Transactional
     void deleteByTargetAndLiker(TT target, User liker);
+    @Query("""
+        SELECT
+            new org.blbulyandavbulyan.blog.dtos.reactions.ReactionStatistics(
+                COALESCE(SUM(CASE WHEN e.liked = true THEN 1 ELSE 0 END), 0),
+                COALESCE(SUM(CASE WHEN e.liked = false THEN 1 ELSE 0 END), 0)
+            )
+        FROM
+            #{#entityName} e
+        WHERE
+        e.target = :target
+    """)
+    ReactionStatistics getStatistics(@Param("target") TT target);
     RT save(RT reaction);
 }
