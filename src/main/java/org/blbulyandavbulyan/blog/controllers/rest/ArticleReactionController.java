61c6a3a7071ad26badbd8dd1ae7c302a821d35d5
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-17 13:15:22 +0400

    added ArticleReactionController#getReactionStatistics

--- src/main/java/org/blbulyandavbulyan/blog/controllers/rest/ArticleReactionController.java
+++ src/main/java/org/blbulyandavbulyan/blog/controllers/rest/ArticleReactionController.java
@@ -3,6 +3,7 @@ package org.blbulyandavbulyan.blog.controllers.rest;
 import lombok.RequiredArgsConstructor;
 import org.blbulyandavbulyan.blog.annotations.validation.article.ValidArticleId;
 import org.blbulyandavbulyan.blog.dtos.reactions.ArticleReactionDTO;
+import org.blbulyandavbulyan.blog.dtos.reactions.ReactionStatistics;
 import org.blbulyandavbulyan.blog.services.reactions.ArticleReactionService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -22,5 +23,9 @@ public class ArticleReactionController {
     @DeleteMapping("/{articleId}")
     public void removeReaction(@PathVariable @ValidArticleId Long articleId, Principal principal){
         articleReactionService.removeReaction(articleId, principal.getName());
+    }
+    @GetMapping("/{articleId}")
+    public ReactionStatistics getReactionStatistics(@PathVariable @ValidArticleId Long articleId){
+        return articleReactionService.getStatistics(articleId);
     }
 }
