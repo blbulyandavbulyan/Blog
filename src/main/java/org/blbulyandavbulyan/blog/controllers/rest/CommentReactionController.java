97a96c4286b4d216318ec2158ef40cd564cdb272
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-28 12:08:12 +0400

    Added CommentReactionController#findReaction, also added CREATED status code on CommentReactionController#createOrAlterReaction, and CommentReactionController#getReactionStatistics now on the other endpoint

--- src/main/java/org/blbulyandavbulyan/blog/controllers/rest/CommentReactionController.java
+++ src/main/java/org/blbulyandavbulyan/blog/controllers/rest/CommentReactionController.java
@@ -3,8 +3,10 @@ package org.blbulyandavbulyan.blog.controllers.rest;
 import lombok.RequiredArgsConstructor;
 import org.blbulyandavbulyan.blog.annotations.validation.comment.ValidCommentId;
 import org.blbulyandavbulyan.blog.dtos.reactions.CommentReactionDTO;
+import org.blbulyandavbulyan.blog.dtos.reactions.ReactionResponse;
 import org.blbulyandavbulyan.blog.dtos.reactions.ReactionStatistics;
 import org.blbulyandavbulyan.blog.services.reactions.CommentReactionService;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,11 +24,18 @@ public class CommentReactionController {
         commentReactionService.removeReaction(commentId, principal.getName());
     }
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void createOrAlterReaction(@Validated @RequestBody CommentReactionDTO commentReactionDTO, Principal principal){
         commentReactionService.react(commentReactionDTO.commentId(), principal.getName(), commentReactionDTO.liked());
     }
-    @GetMapping("/{commentId}")
+    @GetMapping("/statistics/{commentId}")
     public ReactionStatistics getReactionStatistics(@PathVariable @ValidCommentId Long commentId){
         return commentReactionService.getStatistics(commentId);
+    }
+    @GetMapping("/{articleId}")
+    public ReactionResponse findReaction(@PathVariable @ValidCommentId Long articleId, Principal principal){
+        return commentReactionService.getReaction(articleId, principal.getName()).
+                map(r->new ReactionResponse(r.isLiked(), true))
+                .orElseGet(()->new ReactionResponse(false, false));
     }
 }
