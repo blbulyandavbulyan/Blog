2c2f160a1a62f70639d4bdec44e51ce59c2b4f7b
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-28 18:25:49 +0400

    added ArticleReactionController in reactions.js

--- src/main/resources/static/js/reactions.js
+++ src/main/resources/static/js/reactions.js
@@ -38,3 +38,55 @@ app.service('CommentReactionService', function ($http) {
         }
     }
 });
+app.controller('ArticleReactionController', function ($scope, $routeParams, ArticleReactionsService, RoleService){
+    $scope.liked = null;
+    $scope.reactionStatistics = {
+        likesCount: 0,
+        dislikesCount: 0
+    }
+    if($routeParams.articleId) {
+        ArticleReactionsService.getStatistics($routeParams.articleId)
+            .then(
+                function (response) {
+                    const data = response.data;
+                    $scope.reactionStatistics.likesCount = data.likesCount;
+                    $scope.reactionStatistics.dislikesCount = data.dislikesCount;
+
+                }
+            );
+        ArticleReactionsService.getMyReaction($routeParams.articleId)
+            .then(
+                function (response) {
+                    const reaction = response.data;
+                    $scope.liked = reaction.hasReaction ? reaction.liked : null;
+                }
+            );
+    }
+    $scope.canReact = ()=> RoleService.canReactOnArticle();
+    $scope.setLiked = (liked)=>{
+        const articleId = $routeParams.articleId;
+        if($scope.liked === liked){
+            //удаляем реакцию на эту статью
+            ArticleReactionsService.removeReaction(articleId)
+                .then(function (){
+                    if($scope.liked)$scope.reactionStatistics.likesCount--;
+                    else $scope.reactionStatistics.dislikesCount--;
+                    $scope.liked = null;
+                });
+        }
+        else{
+            ArticleReactionsService.react(articleId, liked)
+                .then(function (){
+                    if($scope.liked != null){
+                        if($scope.liked && $scope.reactionStatistics.likesCount > 0)
+                            $scope.reactionStatistics.likesCount--;
+                        else if($scope.reactionStatistics.dislikesCount > 0)
+                            $scope.reactionStatistics.dislikesCount--;
+                    }
+                    if(liked)$scope.reactionStatistics.likesCount++;
+                    else $scope.reactionStatistics.dislikesCount++;
+                    $scope.liked = liked;
+                });
+        }
+    }
+});
