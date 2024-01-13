df694d0c7be71293f95f2fec14472f68aabd674e
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-01-13 16:29:48 +0400

    added implementation for TOTPService

--- src/main/java/org/blbulyandavbulyan/blog/services/TOTPService.java
+++ src/main/java/org/blbulyandavbulyan/blog/services/TOTPService.java
@@ -1,13 +1,40 @@
 package org.blbulyandavbulyan.blog.services;
 
+import dev.samstevens.totp.code.CodeVerifier;
+import dev.samstevens.totp.exceptions.QrGenerationException;
+import dev.samstevens.totp.qr.QrData;
+import dev.samstevens.totp.qr.QrGenerator;
+import dev.samstevens.totp.secret.SecretGenerator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import static dev.samstevens.totp.util.Utils.getDataUriForImage;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TOTPService {
-    //FIXME: 13.01.2024 implement this service
+    private final CodeVerifier codeVerifier;
+    private final SecretGenerator secretGenerator;
+    private final QrGenerator qrGenerator;
+    private final QrData.Builder qrDataBuilder;
+    public String generateNewSecret() {
+        return secretGenerator.generate();
+    }
+
+    public String generateQrCodeImageUri(String secret) {
+        QrData data = qrDataBuilder.secret(secret).build();
+        byte[] imageData;
+        try {
+            imageData = qrGenerator.generate(data);
+        } catch (QrGenerationException e) {
+            throw new RuntimeException(e);
+        }
+        return getDataUriForImage(imageData, qrGenerator.getImageMimeType());
+    }
+
     public boolean verifyCode(String secret, String code) {
-        return false;
+        return codeVerifier.isValidCode(secret, code);
     }
 }
