3dc51413ab48001dc0b8b17c281992f3ba31d2cc
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-01-13 23:22:09 +0400

    added TfaController

---
+++ src/main/java/org/blbulyandavbulyan/blog/controllers/rest/TfaController.java
@@ -0,0 +1,26 @@
+package org.blbulyandavbulyan.blog.controllers.rest;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.blbulyandavbulyan.blog.dtos.authorization.TOTPSetupResponse;
+import org.blbulyandavbulyan.blog.services.TOTPSetupService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/tfa")
+@RequiredArgsConstructor
+@Validated
+public class TfaController {
+    private final TOTPSetupService totpSetupService;
+    @PostMapping
+    public TOTPSetupResponse beginTfaSetup(Principal principal) {
+        return new TOTPSetupResponse(totpSetupService.beginSetupTFA(principal.getName()));
+    }
+    @PatchMapping
+    public void finishSetup(Principal principal, @RequestParam @NotBlank String code) {
+        totpSetupService.finishTFASetup(principal.getName(), code);
+    }
+}
