e72084c5062fdcfaddbe80656a43305fbdfde3ba
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-01-14 14:03:12 +0400

    added UserRepository#isTfaEnabled

--- src/main/java/org/blbulyandavbulyan/blog/repositories/UserRepository.java
+++ src/main/java/org/blbulyandavbulyan/blog/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,4 +23,6 @@ public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificat
     User getReferenceByName(String name);
 
     <T> Optional<T> findByUserId(Long userId, Class<T> dtoClass);
+    @Query("SELECT u.tfaEnabled FROM User u WHERE u.name = :username")
+    Optional<Boolean> isTfaEnabled(@Param("username") String username);
 }
