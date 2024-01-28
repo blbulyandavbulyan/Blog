fbf4d00cbdcfd3aa08146c23222865e001a288e5
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-01-28 18:11:32 +0400

    added @AllArgsConstructor and @NoArgsConstructor

--- src/main/java/org/blbulyandavbulyan/blog/entities/reactions/ReactionId.java
+++ src/main/java/org/blbulyandavbulyan/blog/entities/reactions/ReactionId.java
@@ -1,13 +1,13 @@
 package org.blbulyandavbulyan.blog.entities.reactions;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReactionId implements Serializable {
     private Long target;
     private Long liker;
