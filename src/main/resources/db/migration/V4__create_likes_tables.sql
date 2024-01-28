2f5571c71387fdca2330337b6f2e39b18122e7eb
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-01-28 18:08:29 +0400

    fixed V4__create_likes_tables.sql, removed id field from articles_reactions, comments_reactions, replaced to composite primary key using liker_id and target_id

--- src/main/resources/db/migration/V4__create_likes_tables.sql
+++ src/main/resources/db/migration/V4__create_likes_tables.sql
@@ -1,22 +1,20 @@
 CREATE TABLE articles_reactions
 (
-    id         bigserial primary key,
     liker_id   bigint  NOT NULL,
-    article_id bigint  NOT NULL,
+    target_id  bigint  NOT NULL,
     liked      BOOLEAN NOT NULL,
-    UNIQUE (liker_id, article_id),
+    PRIMARY KEY (liker_id, target_id),
     FOREIGN KEY (liker_id) REFERENCES users (user_id),
-    FOREIGN KEY (article_id) REFERENCES articles (article_id)
+    FOREIGN KEY (target_id) REFERENCES articles (article_id)
 );
 CREATE TABLE comments_reactions
 (
-    id         bigserial primary key,
     liker_id   bigint  NOT NULL,
-    comment_id bigint  NOT NULL,
+    target_id  bigint  NOT NULL,
     liked      BOOLEAN NOT NULL,
-    UNIQUE (liker_id, comment_id),
+    PRIMARY KEY (liker_id, target_id),
     FOREIGN KEY (liker_id) REFERENCES users (user_id),
-    FOREIGN KEY (comment_id) REFERENCES comments (comment_id)
+    FOREIGN KEY (target_id) REFERENCES comments (comment_id)
 );
-CREATE INDEX article_id_in_reactions ON articles_reactions (article_id);
-CREATE INDEX comment_id_in_reactions ON comments_reactions(comment_id);
+CREATE INDEX article_id_in_reactions ON articles_reactions (target_id);
+CREATE INDEX comment_id_in_reactions ON comments_reactions(target_id);
