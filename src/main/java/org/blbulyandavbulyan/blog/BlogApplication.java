4f390ea2929bf5e09d186a4bdbfbf3a7fea3a55f
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-03-31 11:06:24 +0400

    added @OpenApiDefinition annotation for BlogApplication

--- src/main/java/org/blbulyandavbulyan/blog/BlogApplication.java
+++ src/main/java/org/blbulyandavbulyan/blog/BlogApplication.java
@@ -1,8 +1,10 @@
 package org.blbulyandavbulyan.blog;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(info = @Info(title = "Blog API", version = "1.0.0"))
 @SpringBootApplication
 public class BlogApplication {
 
